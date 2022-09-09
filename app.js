569e18cfd2e2cf5de5444579c85c68ee5837a2b2
Jason Lo <jason.chun.man.lo@gmail.com>
2022-09-08 22:49:08 -0400

    Switch to MongoDB via Mongoose to persist data

--- app.js
+++ app.js
@@ -1,42 +1,94 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { default: mongoose, Schema } = require("mongoose");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-
-var items = ["Cook food", "Eat Food"];
-var workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB-solo");
+
+const todoSchema = {
+  item: String,
+};
+
+const listSchema = {
+  name: String,
+  items: [todoSchema],
+};
+
+const ListItem = mongoose.model("Item", todoSchema);
+
+const List = mongoose.model("List", listSchema);
+
+const defaultList = [
+  new ListItem({ item: "This is a to do list!" }),
+  new ListItem({ item: "Add items with the +" }),
+  new ListItem({ item: "Hit the checkbox to delete" }),
+];
+
 app.get("/", function (req, res) {
-  let today = new Date();
+  List.findOne({ name: "ToDo-List" }, (err, list) => {
+    if (!list) {
+      const newList = {
+        name: "ToDo-List",
+        items: defaultList,
+      };
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
+      List.create(newList, (err, doc) => {
+        if (!err) {
+          console.log("Default load success!");
+        }
+      });
 
-  let dateFormatted = today.toLocaleDateString("En-US", options);
-  res.render("index", { title: dateFormatted, myItems: items });
+      setTimeout(() => res.redirect("/"), 100);
+    } else {
+      res.render("index", { title: "ToDo-List", myItems: list.items });
+    }
+  });
 });
 
-app.get("/work", function (req, res) {
-  res.render("index", { title: "Work", myItems: workItems });
+app.get("/:customList", (req, res) => {
+  listName = req.params.customList;
+
+  List.findOne({ name: listName }, (err, list) => {
+    if (!list) {
+      const newList = {
+        name: listName,
+        items: defaultList,
+      };
+
+      List.create(newList, (err, doc) => {
+        if (!err) {
+          console.log("List load success!");
+        }
+      });
+
+      setTimeout(() => res.redirect("/" + listName), 100);
+    } else {
+      res.render("index", { title: listName, myItems: list.items });
+    }
+  });
 });
 
 app.post("/", function (req, res) {
-  let newItem = req.body.addItem;
+  const listName = req.body.listName;
+  const newItem = new ListItem({ item: req.body.addItem });
 
-  if (req.body.button === "Work") {
-    workItems.push(newItem);
-    res.redirect("/work");
+  List.findOne({ name: listName }, (err, list) => {
+    if (list) {
+      list.items.push(newItem);
+      list.save();
+    }
+  });
+
+  if (listName != "ToDo-List") {
+    res.redirect("/" + listName);
   } else {
-    items.push(newItem);
-
     res.redirect("/");
   }
 });
